4b816ece1435699ca34dcf90aef474fcf5a677da
Guido Torres <guido732@gmail.com>
2020-08-17 18:21:32 -0300

    Chore/packages/update packages (#18)
    * chore(package.json) Removes unused dependencies and upgrades dependencies to new version
    
    * 1.1.0

--- database/database.js
+++ database/database.js
@@ -1,11 +1,10 @@
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize("mysql://root:@localhost:3306/delilah_resto");
-// const mysql2 = require("mysql2");
 
 // Async/Await Query Tester
 (async () => {
 	const fetch = await sequelize.query("SELECT * FROM demo", {
-		type: sequelize.QueryTypes.SELECT
+		type: sequelize.QueryTypes.SELECT,
 	});
 	console.log("---- SELECT * FROM demo -----");
 	console.log(fetch);
